faa9493a6a55a74e642ec2b40f794b8d55c7671e
SindhujaDuraisamy <126670915+SindhujaDuraisamy@users.noreply.github.com>
2025-01-20 14:19:07 -0500

    Week1Day1 Assignments

---
+++ day1/Firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f; //(use dataType float)
+	    String browserName = "firefox"; //(use dataType String)
+		boolean isVisible = true;//(use dataType boolean)
+	    int releaseYear = 1998;//(use dataType int)
+	    char browserLogo = 'f'; //(use datatype char)
+	    System.out.println("Browser Version is" +browserVersion);
+	    System.out.println("Browser Name is" +browserName);
+	    System.out.println("Browser Visibility is" +isVisible);
+	    System.out.println("Browser was released in" +releaseYear);
+	    System.out.println("Browser logo is" +browserLogo);
+	    System.out.println(browserVersion + " " + browserName + " " + isVisible + " " + releaseYear + " " + browserLogo);
+
+	}
+
+}
